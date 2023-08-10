5e72d0b621dd1c7c9bd32bf11b5c2ee24b103451
Ivan Iliev <ivan.s.iliev@gmail.com>
2023-08-10 23:07:16 +0300

    Adding comments to HealthServiceViewModel

--- PetHealthCenter.Core/Models/HealthService/HealthServiceViewModel.cs
+++ PetHealthCenter.Core/Models/HealthService/HealthServiceViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using PetHealthCenter.Core.Contracts;
+using System.ComponentModel.DataAnnotations;
+using static PetHealthCenter.Common.Constants.ModelCommentConstants.HealthService;
 
 namespace PetHealthCenter.Core.Models.HealthService
 {
-    internal class HealthServiceViewModel
+    [Comment(ServiceViewModelmain)]
+    public class HealthServiceViewModel : IServiceModel
     {
+        [Comment(ServiceViewModelId)]
+        public int Id { get; set; }
+
+        [Comment(ServiceViewModelName)]
+        public string Name { get; set; } = null!;
+
+        [Comment(ServiceViewModelDescription)]
+        public string Description { get; set; } = null!;
+
+        [Comment(ServiceViewModelPrice)]
+        public decimal Price { get; set; }
+
+        [Comment(ServiceViewModelProductComponentId)]
+        [Display(Name = "Product component")]
+        public int ProductComponentId { get; set; }
+
+        [Comment(ServiceViewModelProductComponent)]
+        public string ProductComponent { get; set; } = null!;
+
+        [Comment(ServiceViewModelProductComponents)]
+        public IEnumerable<HealthServiceProductComponentModel> ProductComponents { get; set; }
+            = new List<HealthServiceProductComponentModel>();
     }
 }
